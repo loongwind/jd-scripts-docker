7963976742c700294f7d538b312fbeaa7841b127
lanfeihu <loyalqx@gmail.com>
2020-12-21 10:29:37 +0800

    Update sync.sh
    switch git

--- sync.sh
+++ sync.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 trap 'cp /jd-scripts-docker/sync.sh /sync' Exit
-git clone --depth=1 https://github.com/loongwind/jd-scripts-docker.git /jd-scripts-docker_tmp
+#git clone --depth=1 https://github.com/loongwind/jd-scripts-docker.git /jd-scripts-docker_tmp
+git clone --depth=1 https://github.com/jk9527/jd-scripts-docker.git /jd-scripts-docker_tmp
 [ -d /jd-scripts-docker_tmp ] && {
   rm -rf /jd-scripts-docker
   mv /jd-scripts-docker_tmp /jd-scripts-docker
 }
-git clone --depth=1 http://git.loongwind.com/cmad/jd_scripts.git /scripts_tmp
+#git clone --depth=1 http://git.loongwind.com/cmad/jd_scripts.git /scripts_tmp
+git clone -b my_master --depth=1 https://gitee.com/jk9527/jd_scripts.git /scripts_tmp
 [ -d /scripts_tmp ] && {
   rm -rf /scripts
   mv /scripts_tmp /scripts
